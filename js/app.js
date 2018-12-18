7580e42d04fba106fd0fd21b8623eb510e8af415
Robert Abreu <robertabreu@pursuit.org>
2018-12-18 16:03:25 -0500

    switched style to materialized, tested grid layout

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+
+// ----------------- Helper Functions ----------------->> >
+const GETRequest = (url, cb) => {
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', response => {
+        const data = JSON.parse(response.currentTarget.response);
+
+        cb(data);
+    });
+    request.send();
+};
+
+const gitRepos = (type) => {
+
+    // const userName = 'FiveEightEight';
+    const readID = '45be248683d35e5b5bca592d5551defc031da312';
+    const ID = `?access_token=${readID}`
+    // const url = `https://api.github.com/users/:${userName}/repos`;
+    //https://api.github.com/users/FiveEightyEight/repos
+    const url = `https://api.github.com/${ID}/users/FiveEightyEight/repos`
+
+    
+
+    GETRequest(url, data => {
+        console.log(data)
+    })
+}
+
+gitRepos();
